9854750cb6fff7e1519642d3e420cc3ed6c3a87d
risky0821-commits <risky0821@gmail.com>
2026-09-08 14:57:35 +0300

    Allow nullable venue fields from API

--- app/src/main/java/com/msdc/baobuzz/models/Venue.kt
+++ app/src/main/java/com/msdc/baobuzz/models/Venue.kt
@@ -4,11 +4,11 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class Venue(
-    val id: Int,
-    val name: String,
-    val address: String?,
-    val city: String,
-    val capacity: Int,
-    val surface: String,
-    val image: String?
+    val id: Int? = null,
+    val name: String? = null,
+    val address: String? = null,
+    val city: String? = null,
+    val capacity: Int? = null,
+    val surface: String? = null,
+    val image: String? = null
 )
